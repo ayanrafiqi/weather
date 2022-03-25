ff6090db82bc792b0c8fb82160806620bdb272ac
ayanrafiqi <rafiqiayan@gmail.com>
2022-03-25 18:14:38 +0530

    Created fronEnd for AWS

---
+++ index.js
@@ -0,0 +1,85 @@
+document.querySelectorAll(".search")[0].addEventListener("click", () => {
+  var cityName = document.querySelector(".city").value;
+  weather(cityName);
+});
+
+async function weather(cityName) {
+  try {
+    var results = await fetch(
+      `https://1yteiky3ul.execute-api.us-east-1.amazonaws.com/test/temp?city=${cityName}`
+    ).then((r) => r.json());
+    //console.log(results);
+    // console.log(Object.keys(results).length);
+    renderResults(results);
+  } catch (err) {
+    console.log("No results found in our database", err);
+    let html = "";
+    let htmlSegment = `<div><h2 style="color :red;text-align :center; font-size:2rem;"> Sorry, No Data Found!! <h2></div>`;
+    html += htmlSegment;
+    addDiv(html);
+  }
+
+  if (results.Temperature >= "20℃" && results.Humidity > "30%") {
+    picture("sunny");
+  } else if (results.Temperature <= "20℃" && results.Precipitation < "50%") {
+    picture("cloudy");
+  } else if (results.Precipitation >= "40%" && results.Temperature <= "20℃") {
+    picture("rainy");
+  }
+}
+
+const renderResults = (results) => {
+  let html = "";
+  var count = Object.keys(results).length;
+  if (results && count === 1) {
+    let htmlSegment = `<div><h2 style="color :red;text-align :center; font-size:2rem;">Sorry, No Data found!!<h2></div>`;
+    html += htmlSegment;
+  }
+
+  if (results && count > 1) {
+    for (const key in results) {
+      let htmlSegment = `<div>
+                   <img src='' alt='' />
+                   <p></p>
+                  <h2>${key} ${results[key]} </h2>
+                      </div>`;
+
+      html += htmlSegment;
+    }
+  }
+
+  addDiv(html);
+ 
+};
+
+function addDiv(html){
+  var divv = document.querySelectorAll("div")[0];
+  divv.classList.add("container");
+  let container = document.querySelector(".container");
+  container.innerHTML = html;
+}
+
+function picture(weatherType) {
+  var weatherRn = `Pictures/${weatherType}.png`;
+  switch (weatherType) {
+    case "sunny":
+      document.querySelectorAll("img")[0].setAttribute("src", weatherRn);
+      document.querySelector("img").classList.add("img");
+      document.querySelector("p").innerHTML = "Sunny";
+      break;
+    case "cloudy":
+      document.querySelectorAll("img")[0].setAttribute("src", weatherRn);
+      document.querySelector("img").classList.add("img");
+      document.querySelector("p").innerHTML = "Cloudy";
+      break;
+
+    case "rainy":
+      document.querySelectorAll("img")[0].setAttribute("src", weatherRn);
+      document.querySelector("img").classList.add("img");
+      document.querySelector("p").innerHTML = "Rainy";
+      break;
+
+    default:
+      console.log();
+  }
+}
