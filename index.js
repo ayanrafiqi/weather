4bcaa846214a4c4a7c63a308fcd8317031404103
ayanrafiqi <rafiqiayan@gmail.com>
2022-04-10 17:05:17 +0530

    added gitignore and gave authorization

--- index.js
+++ index.js
@@ -14,19 +14,17 @@ document.querySelectorAll(".search")[0].addEventListener("click", () => {
   weather(cityName);
 });
 
-/*fetch('https://1yteiky3ul.execute-api.us-east-1.amazonaws.com/test/temp',{
-   method:GET,
+fetch('https://1yteiky3ul.execute-api.us-east-1.amazonaws.com/test/temp',{
+   method:"GET",
     headers:{
-      
-    }
-}*/
-
+      "authentication":accessToken
+    },
+}
+)
 
 async function weather(cityName) {
   try {
-    var results = await fetch(`https://1yteiky3ul.execute-api.us-east-1.amazonaws.com/test/temp?city=${cityName}`,{
-      headers:{"authentication":accessToken}
-    }).then((r) => r.json());
+    var results = await fetch(`https://1yteiky3ul.execute-api.us-east-1.amazonaws.com/test/temp?city=${cityName}`).then((r) => r.json());
     //console.log(results);
     // console.log(Object.keys(results).length);
     renderResults(results);
